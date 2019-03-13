0529bffac3fc9cd6507ff6ce97d59c267cb28551
Matt Robenolt <matt@ydekproductions.com>
2019-03-13 15:24:55 -0700

    Upgrade to go 1.12 and native modules

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -ex
 
 rm -rf bin/
-docker build --rm -t gscp:build .
-docker run --rm -v $PWD/bin:/go/src/app/bin gscp:build
+docker build --pull --rm -t gscp:build .
+docker run --rm -v $PWD/bin:/usr/src/gscp/bin gscp:build
 
 for f in bin/*; do gpg -ab $f; done
---
+++ go.mod
@@ -0,0 +1,8 @@
+module gscp
+
+go 1.12
+
+require (
+	cloud.google.com/go v0.37.0
+	google.golang.org/api v0.1.0
+)
